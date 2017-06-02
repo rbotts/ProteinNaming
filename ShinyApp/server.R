777bd8f8e8beb0049982004be6fcfd9f6427cda7
ZacLindsey <zaclindsey1996@pointloma.edu>
2017-06-02 12:40:36 -0700

    Hide colorbar, Loading message, reformat code

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -11,169 +11,144 @@ library(animation)
 library(ape)  # used for phylogenetic trees
 
 shinyServer(function(input, output) {
-  
-  output$description <- renderUI(
-    {
-      altNames <- as.character(Descriptions$V3[which(Descriptions$V1==input$variable)])
-      
-      if (grepl("\\^",altNames))
-        altNames <- gsub("\\^",", ",altNames)
-      else
-        altNames <- altNames
-      
-      tagList(
-      h1(input$variable, color = "blue"),
-      h4(Descriptions$V2[which(Descriptions$V1==input$variable)]),
-      HTML(paste0("<b>Alternative Names: </b>","<i>",altNames,"</i>"))
-      )
-    }
-  )
+  output$description <- renderUI({
+    altNames <-
+      as.character(Descriptions$V3[which(Descriptions$V1 == input$variable)])
+    
+    if (grepl("\\^", altNames))
+      altNames <- gsub("\\^", ", ", altNames)
+    else
+      altNames <- altNames
+    
+    tagList(h1(input$variable, color = "blue"),
+            h4(Descriptions$V2[which(Descriptions$V1 == input$variable)]),
+            HTML(
+              paste0("<b>Alternative Names: </b>", "<i>", altNames, "</i>")
+            ))
+  })
   output$proteinPlot <- renderPlotly({
     backbone <- input$variable
     
-      # Get the cluster items relevant to the choice of backbone
-      CG2 <- ClusterGroups[which(ClusterGroups$V1 == backbone), ]
-      
-      # Generate a unique color for each cluster
-      cols <- rainbow(length(unique(CG2$V2)))
-      
-      # V6 used to assign colors to each cluster
-      CG2$V6 <- c(1:nrow(CG2))
-      
-      # Assign colors to each clusters
-      index = 1
-      for (i in unique(CG2$V2))
+    # Get the cluster items relevant to the choice of backbone
+    CG2 <- ClusterGroups[which(ClusterGroups$V1 == backbone),]
+    
+    # Generate a unique color for each cluster
+    cols <- rainbow(length(unique(CG2$V2)))
+    
+    # V6 used to assign colors to each cluster
+    CG2$V6 <- c(1:nrow(CG2))
+    
+    # Assign colors to each clusters
+    index = 1
+    for (i in unique(CG2$V2))
+    {
+      # Note that, as we would expect, all of the ones in the same cluster
+      # will have the same color
+      # CG2$V6[which(CG2$V2 == i)] <- cols[index] # old: give unique color to each
+      CG2$V6[which(CG2$V2 == i)] <- index # New: give a unique ID
+      index = index + 1
+    }
+    
+    # Chunk to ensure correct data formatting
+    # These few lines may be unnecessary
+    CG2$V5[which(CG2$V5 == "*")] <- "100.0" #"100.0"
+    CG2$V5 <- as.double(as.character(CG2$V5))
+    CG2$V5[which(is.na(CG2$V5))] <- 0.0
+    CG2$V4 <- as.double(CG2$V4)
+    CG2$V5 <- CG2$V5 / 100.00
+    
+    # Order by either sequence length or cluster
+    if (input$arrange == "Sequence Length")
+    {
+      CG3 <-
+        CG2[order(CG2$V4),]
+    }
+    else
+    {
+      CG3 <- CG2 # This case will "order by cluster."
+    }
+    
+    # V7 is used for labeling
+    CG3$V7 <-
+      c(1:nrow(CG3))
+    
+    # V8 says whether there is a reference plasmid in the *cluster*
+    # V9 says whether the *specific protein* comes from a reference plasmid
+    CG3$V8 <- FALSE
+    CG3$V9 <- FALSE
+    references <- tolower(as.character(References$V1))
+    cids <- c() # Contains cluster IDs
+    for (cRow in c(1:nrow(CG3)))
+    {
+      plasmid <- strsplit(as.character(CG3$V3[cRow]), "_")[[1]]
+      if (length(plasmid) > 2)
       {
-        # Note that, as we would expect, all of the ones in the same cluster
-        # will have the same color
-        # CG2$V6[which(CG2$V2 == i)] <- cols[index] # old: give unique color to each
-        CG2$V6[which(CG2$V2 == i)] <- index # New: give a unique ID
-        index = index + 1
-      }
-      
-      # Chunk to ensure correct data formatting
-      # These few lines may be unnecessary
-      CG2$V5[which(CG2$V5 == "*")] <- "100.0" #"100.0"
-      CG2$V5 <- as.double(as.character(CG2$V5))
-      CG2$V5[which(is.na(CG2$V5))] <- 0.0
-      CG2$V4 <- as.double(CG2$V4)
-      CG2$V5 <- CG2$V5 / 100.00
-      
-      # Order by either sequence length or cluster
-      if (input$arrange == "Sequence Length")
-      {
-        CG3 <-
-          CG2[order(CG2$V4), ]
+        plasmid <- plasmid[3:length(plasmid)]
+        plasmid <- paste(plasmid, collapse = "_")
       }
       else
       {
-        CG3 <- CG2 # This case will "order by cluster."
+        plasmid <- plasmid[2]
       }
-      
-      # V7 is used for labeling
-      CG3$V7 <-
-        c(1:nrow(CG3))
-
-      # V8 says whether there is a reference plasmid in the *cluster*
-      # V9 says whether the *specific protein* comes from a reference plasmid
-      CG3$V8 <- FALSE
-      CG3$V9 <- FALSE
-      references <- tolower(as.character(References$V1))
-      cids <- c() # Contains cluster IDs
-      for (cRow in c(1:nrow(CG3)))
+      if (tolower(plasmid) %in% references)
       {
-        plasmid <- strsplit(as.character(CG3$V3[cRow]), "_")[[1]]
-        if (length(plasmid) > 2)
-        {
-          plasmid <- plasmid[3:length(plasmid)]
-          plasmid <- paste(plasmid, collapse = "_")
-        }
-        else
-        {
-          plasmid <- plasmid[2]
-        }
-        if (tolower(plasmid) %in% references)
-        {
-          cids <- c(cids, CG3$V2[cRow])
-          CG3$V9[cRow] <- TRUE #This *specific protein* comes from a reference
-        }
+        cids <- c(cids, CG3$V2[cRow])
+        CG3$V9[cRow] <-
+          TRUE #This *specific protein* comes from a reference
       }
-      # Note the ones that are in a *cluster* containing a reference plasmid
-      CG3[which(CG3$V2 %in% cids), "V8"] <- TRUE
-      
-      # Take a subset (method from global.R)
-      if (input$Subset)
-      {
-        CG3 <- subsetData(CG3, input$ranges)
-      }
-      
-      # seqData is only used if user tries to download
-      seqData <<-
-        as.character(CG3$V3)
-      
-      pp <- plot_ly(CG3,
-                    y = ~c(1:nrow(CG3)),
-                    x = ~CG3$V4,
-                    type = "bar",
-                    orientation = "h",
-                    color = ~CG3$V6,
-                    hoverinfo = "text",
-                    text = ifelse(CG3$V8,paste0(CG3$V3,""),paste0("<b>",CG3$V3,"\b>"))
-                    ) %>%
-        layout(xaxis = list(title="Sequence Length"),
-               yaxis = list(title="Protein"))
-      
-      # Generate the plot.
-      p<-ggplot(CG3, aes(x = c(1:nrow(CG3)), y = CG3$V4)) + # Notice X position is determined by V7
-        
-        geom_bar(stat = "identity",
-                 fill = CG3$V6) +
-        scale_y_continuous() +
-        scale_x_discrete() + # Remove extra space
-        
-        # # Protein name labels
-        # geom_label(
-        #   size = 1.00,
-        #   y = max(CG3$V4) / 2.0,
-        #   fontface = ifelse(CG3$V8, "bold", "plain"),
-        #   label = CG3$V3,
-        #   color = ifelse(CG3$V9, "red", "black"),
-        #   label.padding = unit(0.05, "lines"),
-        #   label.r = unit(0.05, "lines"),
-        #   label.size = .01,
-        #   alpha = .5
-        # ) +
-        
-        # Bar number labels (somewhat arbitary)
-        geom_text(size = 1.5,
-                  y = -5,
-                  aes(label = rev(CG3$V7)),
-                  color = "black") +
-        coord_flip() +
-        ggtitle(backbone) +
-        labs(y = "Sequence Length", x = "") +
-        theme(axis.text.y = element_text(size = 1),
-              axis.title.y = element_text(size = 2))
-      
-      p<-ggplotly(p, height = 1000)
-      
-      pp
-      
     }
-  )
+    # Note the ones that are in a *cluster* containing a reference plasmid
+    CG3[which(CG3$V2 %in% cids), "V8"] <- TRUE
+    
+    # Take a subset (method from global.R)
+    if (input$Subset)
+    {
+      CG3 <- subsetData(CG3, input$ranges)
+    }
+    
+    # seqData is only used if user tries to download
+    seqData <<-
+      as.character(CG3$V3)
+    
+    pp <- plot_ly(
+      CG3,
+      y = ~ c(1:nrow(CG3)),
+      x = ~ CG3$V4,
+      type = "bar",
+      orientation = "h",
+      color = ~ CG3$V6,
+      hoverinfo = "text",
+      text = ifelse(CG3$V8, paste0(CG3$V3, ""), paste0("<b>", CG3$V3, "</b>"))
+    ) %>%
+      layout(xaxis = list(title = "Sequence Length"),
+             yaxis = list(title = "Protein"))
+    hide_colorbar(pp)
+    
+  })
   
   output$ptrees <- renderPlot({
     # read phylogenetic trees from previously computed newick tree
     # this code may fail if the family only has one cluster
     
-    fpath <- file.path("SeqTreesTest", paste0(input$variable, ".nwk"))
-     validate(
-       need(file.exists(fpath), paste(input$variable, "has 2 or fewer clusters, tree is meaningless"))
-     )
+    fpath <-
+      file.path("SeqTreesTest", paste0(input$variable, ".nwk"))
+    validate(need(
+      file.exists(fpath),
+      paste(
+        input$variable,
+        " tree unavailable due to memory constraints."
+      )
+    ))
     tree <- read.tree(fpath)
-    ggtree(tree) + geom_tiplab(color="purple",hjust = 1.0,vjust=-0.75)+
-    geom_nodepoint(color="#b5e521", alpha=3/8, size=8)+
-    geom_tippoint(color="purple", shape=20, size=4)
+    ggtree(tree) + geom_tiplab(color = "purple",
+                               hjust = 1.0,
+                               vjust = -0.75) +
+      geom_nodepoint(color = "#b5e521",
+                     alpha = 3 / 8,
+                     size = 8) +
+      geom_tippoint(color = "purple",
+                    shape = 20,
+                    size = 4)
     
   })
   
@@ -191,7 +166,8 @@ shinyServer(function(input, output) {
     # the argument 'file'.
     content = function(file) {
       print("READING FASTA... (MAY TAKE UP TO 5 MINUTES)")
-      plasmidAA <- read.fasta("Plasmids20-200kb-6-9-2016AA.fa", "AA")
+      plasmidAA <-
+        read.fasta("Plasmids20-200kb-6-9-2016AA.fa", "AA")
       print("FINISHED FASTA READ")
       
       writeSeqs <- function(seq)
@@ -243,16 +219,13 @@ shinyServer(function(input, output) {
     }
   )
   
-  output$Align <- downloadHandler(
-    
-    filename <- function() {
-      return(paste0(input$variable,".faa"))
-    },
-    
-    content <- function(file){
-      seqAlign <- read.fasta(paste0("SeqAlignments/",input$variable,".fa"), "AA")
-      write.fasta(seqAlign, names = attr(seqAlign,"name"),file)
-    }
-    
-    )
+  output$Align <- downloadHandler(filename <- function() {
+    return(paste0(input$variable, ".faa"))
+  },
+  
+  content <- function(file) {
+    seqAlign <-
+      read.fasta(paste0("SeqAlignments/", input$variable, ".fa"), "AA")
+    write.fasta(seqAlign, names = attr(seqAlign, "name"), file)
+  })
 })
--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -3,50 +3,97 @@ library(plotly)
 #setwd("~/Documents/Projects/BioPythonTools/ProteinNaming-master")
 
 # Load items to be used on both the server and UI, save them to global space
-References <<- read.csv(file="90PlasmidsAndIncGroups.csv",head=FALSE,sep=",")
-ClusterGroups <<- read.csv(file="ClusterGroups_6-1-2017.csv",head=FALSE,sep=",")
+References <<-
+  read.csv(file = "90PlasmidsAndIncGroups.csv", head = FALSE, sep = ",")
+ClusterGroups <<-
+  read.csv(file = "ClusterGroups_6-1-2017.csv", head = FALSE, sep = ",")
 ClusterGroups <<- na.omit(ClusterGroups)
-Descriptions <<- read.csv(file="BackboneDescriptions_6-2-2017.csv",head=FALSE,sep=",")
+Descriptions <<-
+  read.csv(file = "BackboneDescriptions_6-2-2017.csv", head = FALSE, sep =
+             ",")
 
 shinyUI(fluidPage(
-  titlePanel("Plasmid Backbone Families"),
-  sidebarLayout(
-    
-    sidebarPanel(
-      selectInput("variable", "Backbone Protein Name:",
-                choices=names(
-                  setNames(
-                    unique(Descriptions$V1),
-                    unique(Descriptions$V1)
-                  )
-                )
-      ),
-    
-      radioButtons("arrange","Order By:",c("AA Cluster","Sequence Length")),
-      textInput("ranges",label="Select Ranges (1-5, 10-20, ...):"),
-      checkboxInput("Subset", "Only Select These Ranges", FALSE),
-    
-      # To use the local download feature, uncomment the following line
-      # AND add a comma after checkboxInput
-      downloadButton('downloadData', 'Download (may take ~5 min)')
+  tags$head(
+    tags$style(
+      type = "text/css",
+      "#loadmessage {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      ocacity: 0.50;
+      text-align: center;
+      font-weight: bold;
+      font-size: 300%;
+      color: #000000;
+      z-index: 105;
+      animation: blinker 1s linear infinite;
+      }"
+)
     ),
+
+titlePanel("Plasmid Backbone Families"),
+sidebarLayout(
+  sidebarPanel(
+    selectInput("variable", "Backbone Protein Name:",
+                choices = names(setNames(
+                  unique(Descriptions$V1),
+                  unique(Descriptions$V1)
+                ))),
+    
+    radioButtons("arrange", "Order By:", c("AA Cluster", "Sequence Length")),
+    textInput("ranges", label = "Select Ranges (1-5, 10-20, ...):"),
+    checkboxInput("Subset", "Only Select These Ranges", FALSE),
+    
+    # To use the local download feature, uncomment the following line
+    # AND add a comma after checkboxInput
+    downloadButton('downloadData', 'Download (may take ~5 min)')
+  ),
   
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Plot of protein families",
-                uiOutput("description"),
-                 plotlyOutput("proteinPlot")
+  mainPanel(tabsetPanel(
+    tabPanel(
+      "Plot of protein families",
+      
+      conditionalPanel(
+        condition = "$('html').hasClass('shiny-busy')",
+        tags$div("Loading...", id = "loadmessage"),
+        tags$script(
+          HTML(
+            "
+            (function blink() {
+            $('#loadmessage').fadeOut(500).fadeIn(500, blink);
+            })();
+            "
+          )
+        )
         ),
-        tabPanel("Alignments", 
-                 h5("Download and reopen the linked alignment after updating the protein family."),
-                 h5("Proteins with more than 100 protein families, may not have alignments due to memory constraints."),
-                 downloadButton("Align","Download alignment")
+      
+      uiOutput("description"),
+      plotlyOutput("proteinPlot")
+          ),
+    tabPanel(
+      "Alignments",
+      h5(
+        "Proteins with more than 100 families may not have alignments due to memory constraints."
+      ),
+      downloadButton("Align", "Download alignment")
+    ),
+    tabPanel(
+      "Phylogenetic trees",
+      conditionalPanel(
+        condition = "$('html').hasClass('shiny-busy')",
+        tags$div("Loading...", id = "loadmessage"),
+        tags$script(
+          HTML(
+            "
+            (function blink() {
+            $('#loadmessage').fadeOut(500).fadeIn(500, blink);
+            })();
+            "
+          )
+        )
         ),
-        tabPanel("Phylogenetic trees",
-                 h5("Computing phylogenetic tree may take a few minutes.  "),
-                 plotOutput("ptrees"))
-      )
+      plotOutput("ptrees")
+          )
+        ))
     )
-  )
-)
-)
+    ))
